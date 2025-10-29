f8a31d7624f530f221eaeda22509662ddccfbd7f
aldys002 <igidiaaldys@gmail.com>
2025-10-29 21:45:39 +0700

    menambah bagian put agar bisa validasi sesuai tanggal

--- 20230140207-node-server/routes/presensi.js
+++ 20230140207-node-server/routes/presensi.js
@@ -1,10 +1,34 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const presensiController = require("../controllers/presensiController");
-const { addUserData } = require("../middleware/permissionMiddleware");
+const presensiController = require('../controllers/presensiController');
+const { addUserData } = require('../middleware/permissionMiddleware');
 
-router.use(addUserData);
-router.post("/check-in", presensiController.CheckIn);
-router.post("/check-out", presensiController.CheckOut);
+router.post("/check-in", addUserData, presensiController.CheckIn);
+router.post("/check-out", addUserData, presensiController.CheckOut);
+router.put(
+  "/:id",
+  [
+    addUserData,
+    body("waktuCheckIn")
+      .isISO8601()
+      .withMessage("waktuCheckIn harus dalam format tanggal valid (ISO8601)"),
+    body("waktuCheckOut")
+      .isISO8601()
+      .withMessage("waktuCheckOut harus dalam format tanggal valid (ISO8601)"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  presensiController.updatePresensi
+);
+
+router.delete("/:id", addUserData, presensiController.deletePresensi);
+
+router.get("/reports/daily", presensiController.getDailyReport);
+
 
 module.exports = router;
