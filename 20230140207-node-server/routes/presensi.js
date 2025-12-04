b77c5efe6a0a572f05a01caa817f3a07b4f43809
aldys002 <igidiaaldys@gmail.com>
2025-12-04 17:23:07 +0700

    update presensi.js

--- 20230140207-node-server/routes/presensi.js
+++ 20230140207-node-server/routes/presensi.js
@@ -4,17 +4,27 @@ const router = express.Router();
 const presensiController = require("../controllers/presensiController");
 const { authenticateToken } = require("../middleware/permissionMiddleware");
 
-// CHECK-IN (DENGAN UPLOAD SELFIE)
+// CHECK-IN (WAJIB FOTO)
 router.post(
   "/check-in",
-  [authenticateToken, presensiController.upload.single("image")],
+  authenticateToken,
+  presensiController.upload.single("buktiFoto"),
   presensiController.CheckIn
 );
 
-// CHECK-OUT
-router.post("/check-out", authenticateToken, presensiController.CheckOut);
 
-// GET DAILY REPORT
-router.get("/daily", authenticateToken, presensiController.getDailyReport);
+// CHECK-OUT
+router.post(
+  "/check-out",
+  authenticateToken,
+  presensiController.CheckOut
+);
+
+// DAILY REPORT
+router.get(
+  "/daily",
+  authenticateToken,
+  presensiController.getDailyReport
+);
 
 module.exports = router;
